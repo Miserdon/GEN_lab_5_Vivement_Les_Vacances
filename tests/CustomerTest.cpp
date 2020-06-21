c016b861c70d528868c6047850f7ec87e3987f8d
Robin Cuénoud <robin.cuenoud@heig-vd.ch>
2020-06-21 18:59:53 +0200

    first test writtteen

--- tests/CustomerTest.cpp
+++ tests/CustomerTest.cpp
@@ -1,5 +1,22 @@
-//
-// Created by robin on 21.06.20.
-//
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Customer.h"
+#include "Rental.h"
 
-#include "CustomerTest.h"
+TEST(Customer, statement) {
+
+    Customer customer("Olivier");
+    customer.addRental( Rental( Movie("Karate Kid"), 7));
+    customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+    customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+    std::string expected = "Rental Record for Olivier\n"
+                           "\tKarate Kid\t9.5\n"
+                           "\tAvengers: Endgame\t15\n"
+                           "\tSnow White\t1.5\n"
+                           "Amount owed is 26\n"
+                           "You earned 4 frequent renter points";
+
+    ASSERT_EQ(expected, customer.statement());
+}

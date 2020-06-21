13dd35f3f5c5f0d8b9b849609c9ac4fd488888ff
Robin Cuénoud <robin.cuenoud@heig-vd.ch>
2020-06-21 21:49:51 +0200

    Clang-Tidy: Constructors that are callable with a single argument must be marked explicit to avoid unintentional implicit conversions

--- Movie.h
+++ Movie.h
@@ -10,7 +10,7 @@ public:
     static const int REGULAR     = 0;
     static const int NEW_RELEASE = 1;
 
-    Movie( std::string  title, int priceCode = REGULAR );
+    explicit Movie( std::string  title, int priceCode = REGULAR );
 
     int getPriceCode() const;
 
